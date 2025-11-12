5ad5462ff1102f3e375c8e561f85bfa7d5a04b98
cliu239 <cliu239@uic.edu>
2025-11-11 22:13:54 -0600

    Updated working way of doing conversin from h5ad to matriviz files.

--- src/h5ad_file.py
+++ src/h5ad_file.py
@@ -1,8 +1,18 @@
 import anndata as ad
+import h5py
+
+
+def print_h5_structure(name, obj):
+    if isinstance(obj, h5py.Dataset):
+        print(f"📄 Dataset: {name}, shape={obj.shape}, dtype={obj.dtype}")
+    elif isinstance(obj, h5py.Group):
+        print(f"📂 Group: {name}")
+
+path = r"C:\Users\Orangeeee\Downloads\expression_matrices.h5"
+with h5py.File(path, "r") as f:
+    print(f"=== File: {path} ===")
+    f.visititems(print_h5_structure)
+
 
 # read the .h5ad file
 # adata = ad.read_h5ad(r"C:\Users\Orangeeee\Downloads\expr.h5ad")
-adata = ad.read_h5ad(r"C:\Users\Orangeeee\Downloads\0.h5ad")
-
-# view basic info
-print(adata)
